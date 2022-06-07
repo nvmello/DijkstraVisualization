3ad774071ddd4d4e2dd8cf78024020e698978066
nvmello <47268886+nvmello@users.noreply.github.com>
2022-06-07 12:41:19 -0700

    Add files via upload

--- dijkstra/main.js
+++ dijkstra/main.js
@@ -41,50 +41,68 @@ scene.add(gridHelper);   //adds the grid and light helpers to the scene
 const controls = new OrbitControls(camera, renderer.domElement);    //allows the user to to control screen with mouse
 //***************************************************************************** */
 
-
-var nodeNum = 1   //keeps track of the node number for naming
+var nodeNum = 1;
 const gui = new GUI()     //create a new gui element
 var nodeFolder = gui.addFolder('Nodes'); 
+
+
+/**
+ * new custom Node class that provides the necessary variables to perform dijkstras
+ */
+class Node {
+  constructor(color) {
+    const geometry = new THREE.SphereGeometry(2,24,24);      //creates a sphere geometry
+    const material = new THREE.MeshStandardMaterial({color: color}); //sets the sphere material
+    const node = new THREE.Mesh(geometry, material);    //creates a new 'node' onject with both the geometry and material specified
+    this.x = THREE.MathUtils.randFloatSpread(100) ;  //randomly generates a unique x,y,z value for each star
+    this.y = THREE.MathUtils.randFloatSpread(100) ;  //randomly generates a unique x,y,z value for each star
+    this.z = THREE.MathUtils.randFloatSpread(100) ;  //randomly generates a unique x,y,z value for each star
+    console.log(this.x);
+    this.visited = 0;   //this variable will be used in dijkstras to tell if the nod has been visited yet
+    this.distance = Infinity;     //property of dijkstras, begin will all nodes distance being set to infinity
+
+    var nodeName = "Node " + nodeNum  //Names each node for the gui
+    console.log(nodeName);  //test code for the console
+    nodeFolder = gui.addFolder(nodeName);   //adds folders to the gui window
+  
+    //I am hoping to be able to use this part of the code to allow the user to determine the weights of edges
+      //may have to move this to the addEdge() function
+    nodeFolder.add(node.rotation, 'x', 0, 10); //adds a node folder for each node created, 
+  
+    nodeNum+=1;   //increment node name before creating a new node
+
+    node.position.set(this.x,this.y,this.z);   //sets each stars position to the randomly generated x,y,z position calculated previously
+    scene.add(node);    //adds each star to the scene once it is created
+  }
+}
+
+var nodeArray = []; //array to store the vector3 positions of each node
+
 
 /**
  * addNode()
  * function will create a node every time called
  */
- function addNode(){
-   const geometry = new THREE.SphereGeometry(2,24,24);      //creates a sphere geometry
-   const material = new THREE.MeshStandardMaterial({color: 0xffff00}); //sets the sphere material
-
-   const node = new THREE.Mesh(geometry, material);    //creates a new 'node' onject with both the geometry and material specified
-
-  const [ x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100) );  //randomly generates a unique x,y,z value for each star
-  node.position.set(x,y,z);   //sets each stars position to the randomly generated x,y,z position calculated previously
-
-  
-  var nodeName = "Node " + nodeNum  //Names each node for the gui
-  console.log(nodeName);  //test code for the console
-  nodeFolder = gui.addFolder(nodeName);   //adds folders to the gui window
-
-  //I am hoping to be able to use this part of the code to allow the user to determine the weights of edges
-    //may have to move this to the addEdge() function
-    nodeFolder.add(node.rotation, 'x', 0, 10); //adds a node folder for each node created, 
-
-    nodeNum+=1;   //increment node name before creating a new node
-    scene.add(node);    //adds each star to the scene once it is created 
-    return new THREE.Vector3(node.position.x,node.position.y,node.position.z);    //return vec3 of positions in order to create edges later
+function addNode(){
+  for( var i = 0; i < 25; i++){ //loop to fill nodeArray
+    const myNode = new Node(0x0000ff);
+    // console.log("myNode.x");
+    // console.log(myNode.x);
+    nodeArray[i] = new THREE.Vector3(myNode.x, myNode.y, myNode.z);
+  }
 }
 
+//call addNode() function
+addNode();
 
-var nodeArray = []; //array to store the vector3 positions of each node
-
-for( var i = 0; i < 5; i++){ //loop to fill nodeArray
-  nodeArray[i] = addNode();
-}
 
 /**
  * addEdge()
  * function will create one edge between two nodes each time called
  */
 //  function addEdge(vecP1, vecP2, color){
+//   //  console.log(vecP1);
+//   //  console.log(vecP2);
 //   const material = new THREE.LineBasicMaterial( { color: color, linewidth: 6 } );  //creates line material, linewidth will not work because of opengl
 //   const points = [];  //create an array of points the edge will travel
 //   points.push( vecP1 );   //vector of endpoints from nodeA
@@ -95,14 +113,16 @@ for( var i = 0; i < 5; i++){ //loop to fill nodeArray
 // }
 
 
+
+
 //******************************PRACTICE ARROW EDGE************************************** */
 function addEdge(vecP1, vecP2, color){
 
-/**
- * .clone ( recursive : Boolean ) : Object3D
- *    recursive -- if true, descendants of the object are also cloned. Default is true.
- *    Returns a clone of this object and optionally all descendants.
- */
+// /**
+//  * .clone ( recursive : Boolean ) : Object3D
+//  *    recursive -- if true, descendants of the object are also cloned. Default is true.
+//  *    Returns a clone of this object and optionally all descendants.
+//  */
   var direction = vecP2.clone().sub(vecP1);     //subtracts vecP1 from vecP2
   var length = direction.length();  //calculates the distance between 2 nodes
   var arrowHelper = new THREE.ArrowHelper(direction.normalize(), vecP1, length, color );  //creates the arrowHelper
@@ -112,22 +132,20 @@ function addEdge(vecP1, vecP2, color){
 
 
 // Test code to connect each node
-// addEdge(nodeArray[0], nodeArray[1]);
+/**
+ * Ideally I would like to assure every node is reachable from every other node,
+ *    so this should not be random
+ */
 for(var i = 0; i<nodeArray.length; i++){
-  for(var j = 0; j<nodeArray.length; j++){
-    if(i != j){
-      if(j %2 == 0){
-        addEdge(nodeArray[i], nodeArray[j], 0xff00ff);
-      } else{
-        addEdge(nodeArray[i], nodeArray[j], 0x0000ff);
-      }
-      
-    }
-  }
-}
+  // console.log("i = " + i);
+  
+  var rand1 = nodeArray[Math.floor(Math.random()*nodeArray.length)];    //select random node to begin an edge
+  var rand2 = nodeArray[Math.floor(Math.random()*nodeArray.length)];    //select a random node for the created edge to travel to
+  // console.log("rand1: " + rand1 );
+  // console.log("rand2: " + rand2 );
 
-
-// addEdge(nodeArray[0], nodeArray[1]);  //code to connect node 1 with node 2
+  addEdge(rand1, rand2, 0xff0000);    //add the randomly created edge to the scene
+} 
 
 
 
