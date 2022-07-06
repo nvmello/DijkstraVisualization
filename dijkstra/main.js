b2eb9a7624fe15eb963cfc3e10ca45ed4fafaffd
Nicholas Morello <nickvmorello@gmail.com>
2022-07-06 15:20:43 -0700

    ready for heroku

--- dijkstra/main.js
+++ dijkstra/main.js
@@ -2,7 +2,6 @@ import * as THREE from '/node_modules/three';
 import { OrbitControls } from '/node_modules/three/examples/jsm/controls/OrbitControls.js';
 
 
-
 /**
  ******************************* Scene setup *********************************
  */
