ea203a7d318a8c07d0ff67eaea0ff3fdace3ee45
Nicholas Morello <nickvmorello@gmail.com>
2022-07-06 14:40:48 -0700

    ready for heroku

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 var http = require('http');
 var server = http.Server(app);
 
-app.use(express.static('dijkstra'));
+app.use(express.static('dijkstra' + '/public'));
 
 server.listen(PORT, function() {
   console.log('Dijkstra Visualization Running');
