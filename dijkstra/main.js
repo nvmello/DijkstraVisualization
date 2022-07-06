6e32dd9fb7620ae33edd01573345c3fb2021f6c8
Nicholas Morello <nickvmorello@gmail.com>
2022-07-06 14:22:51 -0700

    ready for heroku

--- dijkstra/main.js
+++ dijkstra/main.js
@@ -1,7 +1,7 @@
 import './style.css'
 import * as THREE from 'three';
 import { GUI } from 'dat.gui'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from 'dijkstra\node_modules\three\examples\jsm\controls\OrbitControls.js';
 
 
 
