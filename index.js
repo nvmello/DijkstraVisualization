614688d7caee08547e80fccc1484ebadcec415c3
Nicholas Morello <nickvmorello@gmail.com>
2022-07-06 16:47:04 -0700

    undo most recent push

--- index.js
+++ index.js
@@ -11,39 +11,3 @@ server.listen(PORT, function() {
   console.log('Dijkstra Visualization Running');
 });
 
-const express = require('express')
-const app = express()
-
-app.use((req, res, next) => {
-  console.log('Time:', Date.now())
-  next()
-})
-
-app.use('/user/:id', (req, res, next) => {
-    console.log('Request Type:', req.method)
-    next()
-  })
-
-  app.get('/user/:id', (req, res, next) => {
-    res.send('USER')
-  })
-
-  app.use('/user/:id', (req, res, next) => {
-    console.log('Request URL:', req.originalUrl)
-    next()
-  }, (req, res, next) => {
-    console.log('Request Type:', req.method)
-    next()
-  })
-
-  app.get('/user/:id', (req, res, next) => {
-    console.log('ID:', req.params.id)
-    next()
-  }, (req, res, next) => {
-    res.send('User Info')
-  })
-  
-  // handler for the /user/:id path, which prints the user ID
-  app.get('/user/:id', (req, res, next) => {
-    res.send(req.params.id)
-  })
