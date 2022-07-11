263c4eb5eface193f41dbc62c64b178a78cc906e
Nick Morello <nickvmorello@gmail.com>
2022-07-11 12:37:44 -0700

    console log to html

--- dijkstra/main.js
+++ dijkstra/main.js
@@ -4,6 +4,18 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'OrbitControls';
 
+(function () {
+  var old = console.log;
+  var logger = document.getElementById('log');
+  console.log = function (message) {
+    if (typeof message == 'object') {
+      logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : message) + '<br />';
+    } else {
+      logger.innerHTML += message + '<br />';
+    }
+  }
+})();
+
 /**
  ******************************* Scene setup *********************************
  */
